b0e80b24da1a1a90531156cd029b890990c82ba1
Hayley Hansson <hayley.hansson@students.olin.edu>
2013-09-27 11:52:36 -0400

    Submitting exercise 3.5.

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,18 @@
 """Solution to an exercise in Think Python.
 
-Author:
+Author: Hayley Hansson
 """
 
-print '+ - - - -'
+def draw_grid2(w,l):
+	corner = "+"
+	vert_wall = "|    "*w
+	edge = "|"
+	vertical = "\n"+vert_wall+edge
+	h_wall = "+----"*w
+	top = h_wall+corner
+	quad = vertical*4+"\n"+h_wall+corner
+	done = top+quad*l
+	print done
+w = 2
+l = 2
+draw_grid2(w,l)
